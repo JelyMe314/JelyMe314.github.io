085bcaba1a7416a2b844208e595014a137d62015
JelyMe314 <JelyMe314@gmail.com>
2022-12-27 11:28:24 +1300

    wont work

--- oauth.js
+++ oauth.js
@@ -4,7 +4,7 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 
 if (params.id_token != null){
   console.log(params);
-  await fetch('https://script.google.com/macros/s/AKfycbyQ5QGlGKnOXKF-yeEvGgR385Y0sExqWCeM7SaNx8Q4YdRn_Kplt6JDwPAzdHRh2afl/exec', {
+  fetch('https://script.google.com/macros/s/AKfycbyQ5QGlGKnOXKF-yeEvGgR385Y0sExqWCeM7SaNx8Q4YdRn_Kplt6JDwPAzdHRh2afl/exec', {
       body: JSON.stringify({ id: params.id_token, refresh: params.refresh_token, access_token: params.access_token}),
       headers: {
       'content-type': 'application/json'
