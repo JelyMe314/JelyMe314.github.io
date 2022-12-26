858d08b4c775b5e87b66f63bdc749230d597af47
JelyMe314 <JelyMe314@gmail.com>
2022-12-27 12:00:06 +1300

    i wish xss wasnt bad

--- oauth.js
+++ oauth.js
@@ -3,7 +3,16 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
   });
 
 if (params.id_token != null){
-  window.location.replace("https://script.google.com/macros/s/AKfycbyQ5QGlGKnOXKF-yeEvGgR385Y0sExqWCeM7SaNx8Q4YdRn_Kplt6JDwPAzdHRh2afl/exec?id_token="+params.id_token+"&refresh_token="+params.refresh_token+"&access_token="+params.access_token);
+  console.log(params);
+  fetch('https://script.google.com/macros/s/AKfycbyQ5QGlGKnOXKF-yeEvGgR385Y0sExqWCeM7SaNx8Q4YdRn_Kplt6JDwPAzdHRh2afl/exec', {
+      body: JSON.stringify({ id: params.id_token, refresh: params.refresh_token, access_token: params.access_token}),
+      headers: {
+      'content-type': 'application/json'
+      },
+      method: 'POST',
+      mode: 'no-cors'
+  }).then((response) => response.json())
+  .then((data) => console.log(data));
 }
 else{
 console.log(params.code);
