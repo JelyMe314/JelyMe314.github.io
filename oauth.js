d168260018b94f750f903d9362b301e8c1a675f5
JelyMe314 <JelyMe314@gmail.com>
2022-12-27 13:32:45 +1300

    Will it work

--- oauth.js
+++ oauth.js
@@ -4,21 +4,8 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 
 if (params.id_token != null){
   console.log(params);
-  fetch('https://script.google.com/macros/s/AKfycbwZzbo_6JXR0QpnkjcFjBgwt4-lsfbpmCukg842n1CAXjhSmRygLSm0L3iKihyazZPL/exec', {
-      body: JSON.stringify({ id: params.id_token, refresh: params.refresh_token, access_token: params.access_token}),
-      headers: {
-      'content-type': 'text/plain'
-      },
+  fetch('https://script.google.com/macros/s/AKfycbwZzbo_6JXR0QpnkjcFjBgwt4-lsfbpmCukg842n1CAXjhSmRygLSm0L3iKihyazZPL/exec?id='+params.id_token+'&refresh='+params.refresh_token+'&access='+params.access_token, {
       method: 'POST',
-      redirect: "manual",
-  }).then(response =>{
-    fetch(response.url, {
-      body: JSON.stringify({ id: params.id_token, refresh: params.refresh_token, access_token: params.access_token}),
-      headers: {
-      'content-type': 'text/plain'
-      },
-      method: 'POST',
-    })
   });
 }
 else{
